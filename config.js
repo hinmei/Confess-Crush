0c6a631cf90782000c7f130f2cc842cb79d87e19
hinmei <91963191+hinmei@users.noreply.github.com>
2021-10-20 20:57:47 +0700

    Dzahan

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "heloo Yêu",
-    introTitle: 'Nọc Chút',
+    titleWeb: "heloo gia sư :3",
+    introTitle: 'Ngọc Lingg',
     introDesc: `Trái đất vốn lạ thường
     Mà sao chị cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^nhấn vào đây nè bà chị già^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: '^^nhấn vào đây nè gia sư khó tính^^',
+    title: 'Dạo này chị có hay bị nhớ tới em khum?',
+    desc: 'Còn em thì dạo này hay nhớ tới chị lắm :3',
+    btnYes: 'Có chứ <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho người yêu của chị <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Mình còn cơ hội với nhau không chị ?',
+    btnReply: 'Gửi cho Dzahan <3',
+    reply: 'có nha Dzahan đáng yêu :3',
     mess: 'Mình biết mà 🥰. Bạn yêu mình nhiều hehe biết ngay mà mình cũng zậy 😘😘',
-    messDesc: 'Ngày nay làm việc có mệt ko ? Đương nhiên là có rồi nhưng mình cũng chỉ biết vậy thôi chứ có làm được gì đâu. Chỉ muốn nói yêu bạn nhiều nhiều ớiiii là nhieều. Gửi triệu tymmmmm từ ĐakLak tới Sài Gòn nơi chị :3 .',
-    btnAccept: 'Yêu Mei :3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Mình chia tay cũng được hơn 1 tháng rồi chị nhỉ. Dù vậy nhưng em luôn nghĩ tới chị. Mặc dù, em biết bây giờ nói cũng không có ý nghĩa gì. Nhưng giờ không nói thì em sợ phải nhìn chị bên cạnh người khác. Em muốn hỏi chị là: Mình quay lại nha :3',
+    btnAccept: 'Oki lunnn :3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //
 }
